7cc6966b14b8463fc0d3a725fd9cb7e076a2d164
liadomke <lia.domke@noaa.gov>
2025-04-22 16:16:46 -0800

    Update year & gear range and filters
    1. Changed year range to default between 2002 - 3000 and added a stop error message to year if the year input year doesn't fall within the acceptable range (2002 to today)
    
    2. Changed gear type to default to CAN trawl and adjusted acceptable gear vectors to only those in the catch table (oceanography joins will be a diff function). Added gear type check that stops pull if the correct types aren't present.
    
    3. Added calculation of tow duration (haulback - eq time) which required converting times with lubridate.
    
    4. Adjusted output columns in the return(data) to included commonly requested columns (oceanographic domain, lme, nbs strata).

--- R/join_event_catch.R
+++ R/join_event_catch.R
@@ -6,19 +6,41 @@
 #' @export
 
 
-join_event_catch <- function(start_year=1990, end_year=3000, tsn=NA, gear=NA) {
+join_event_catch <- function(start_year=2002, end_year=3000, tsn=NA,
+                             gear= "CAN") {
   # download tables from AKFIN
-  event <- get_ema_event() |> dplyr::rename_with(tolower)
-  catch <- get_ema_catch() |> dplyr::rename_with(tolower)
+  event <- get_ema_event() |> dplyr::rename_with(tolower) |>
+    dplyr::mutate(gear = ifelse(gear == "NOR64", "Nor64", gear)) # fix gear typo in db
+  catch <- get_ema_catch() |> dplyr::rename_with(tolower) |>
+    dplyr::mutate(gear = ifelse(gear == "NOR64", "Nor64", gear)) # fix gear typo in db
   taxa <- get_ema_taxonomy() |> dplyr::rename_with(tolower)
   event_parameters <- get_ema_event_parameters() |> dplyr::rename_with(tolower)
 
-  # optional gear filter
-  if(is.na(gear)) {
-    gear_vec<- unique(event$gear)
+  unique.gears <- unique(catch$gear)
+
+  # gear filter - only allow gears present in catch table
+  if(all(gear %in% unique(catch$gear))){
+    gear_vec <- c(gear)
   } else {
-    gear_vec<-c(gear)
+    stop("Gear type must be CAN, MAR, NETS156, Nor264")
   }
+
+  # if(is.na(gear)) {
+  #   gear_vec <- unique(catch$gear)
+  # } else {
+  #   gear_vec <- c(gear)
+  # }
+
+  # error message if start year not within range
+  if(start_year < 2002) {
+    stop("Start year not within acceptable range, try 2002 - present")
+  }
+    # error message if end year not within range
+  if(end_year < 2002) {
+    stop("Start year not within acceptable range, try 2002 - present")
+  }
+
+
 
   # optional tsn filter
   if(is.na(tsn)) {
@@ -36,9 +58,15 @@ join_event_catch <- function(start_year=1990, end_year=3000, tsn=NA, gear=NA) {
            & gear %in% gear_vec) |>
     dplyr::left_join(catch2, by=c("station_id"="station_id", "event_code"="event_code", "gear"="gear")) |>
     dplyr::left_join(event_parameters, by=c("station_id"="station_id", "event_code"="event_code", "gear"="gear")) |>
-    dplyr::select(sample_year, event_code, station_id, gear, gear_performance, tow_type, eq_latitude, eq_longitude, effort, 
-                  effort_units, species_tsn, common_name, scientific_name, lhs_code, total_catch_number, total_catch_weight)
+    dplyr::mutate(haulback_time = lubridate::ymd_hms(haulback_time),
+                  eq_time = lubridate::ymd_hms(eq_time),
+                  tow_duration = difftime(haulback_time, eq_time, units = "mins")) |>
+    dplyr::select(sample_year, cruise_id, event_code, station_id, gear, gear_performance, tow_type, nbs_strata, oceanographic_domain,
+                  large_marine_ecosystem, eq_latitude, eq_longitude,
+                  effort, effort_units, tow_duration,
+                  species_tsn, common_name, scientific_name, lhs_code, total_catch_number, total_catch_weight)
 
+  print(paste("Last AKFIN catch upload date", unique(catch$akfin_load_date)))
   return(data)
 }
 
