c3bf73798289a44e176d49b5b33268342a062b9e
liadomke <lia.domke@noaa.gov>
2025-07-07 11:18:49 -0800

    Add error message if akfin database connection fails

--- R/get_ema_catch.R
+++ R/get_ema_catch.R
@@ -23,32 +23,4 @@ get_ema_catch <- function() {
 }
 
 
-#start_year= 2024; end_year = 2024; tsn ="all"; gear = "all"
 
-# get_ema_catch <- function(start_year, end_year, tsn, gear) {
-#   # based on Matt Callahan's code
-#   # define queries to pass to url (akfin api)
-#   # remove parameters that should return all values
-#   query <- list(
-#     tsn = if (length(tsn) == 1 && tsn == "all") NULL else paste(tsn, collapse=","),
-#     gear = if (length(gear) == 1 && gear == "all") NULL else paste(gear, collapse=","),
-#     start_year = start_year,
-#     end_year = end_year
-#   )
-#   # Remove NULL entries from the query list
-#   query <- query[!sapply(query, is.null)]
-#
-#   # base url for the api pull
-#   url <- "https://apex.psmfc.org/akfin/data_marts/ema/event_catch?"
-#
-#   # basic function to pull a url
-#   response <- httr::GET(url=url, query=query)
-#
-#   # use jasonlite and the parameters we are setting above to pull data
-#   data <- jsonlite::fromJSON(
-#     httr::content(response, type = "text", encoding = "UTF-8")) |>
-#     dplyr::bind_rows()
-#
-#   return(data)
-#
-# }
