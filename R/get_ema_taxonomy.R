3eda0fbeb2ec6ea353a0d28c11d3c3c0543838d2
LiaDomke-NOAA <168690819+liadomke@users.noreply.github.com>
2025-11-13 10:46:29 -0900

    Fix upside down ? in get_ema_taxonomy.R
    Following Matt Callahan's submitted issue - revised code to fix appearance of upside ? on taxonomic names when pulled from akfin

--- R/get_ema_taxonomy.R
+++ R/get_ema_taxonomy.R
@@ -5,18 +5,25 @@
 #' @export
 
 
-get_ema_taxonomy <- function() {
+get_ema_taxonomy <- function() get_ema_taxonomy <- function() {
   url <- "https://apex.psmfc.org/akfin/data_marts/ema/lut_trawl_species_tsn?"
-  #basic function to pull a url
-  response <- httr::GET(url=url)
+  
+  # Fetch as UTF-8 text (note: 'as', not 'type')
+  resp_txt <- httr::content(httr::GET(url), as = "text", encoding = "UTF-8")
+  
+  dat <- jsonlite::fromJSON(resp_txt, flatten = TRUE)
+  
+  # Replace U+00BF with regular spaces in character columns
+  dat <- dplyr::mutate(
+    dat,
+    dplyr::across(
+      where(is.character),
+      ~ gsub("\u00BF", " ", .x, fixed = TRUE)
+    )
+  )
+  
+  dat <- dplyr::rename_with(dat, tolower)
 
-  # use jasonlite and the parameters we are setting above to pull data
-  data <- jsonlite::fromJSON(
-    httr::content(response, type = "text", encoding = "UTF-8")) |>
-    dplyr::bind_rows() |>
-    dplyr::rename_with(tolower) # rename to lower
-
-  return(data)
-
+  return(dat)
 }
 
