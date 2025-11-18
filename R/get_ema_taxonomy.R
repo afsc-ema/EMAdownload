87b592b0a73c33ca6e2428043b8fd1f77d12e582
liadomke <lia.domke@noaa.gov>
2025-11-17 16:13:05 -0900

    fixed typo in get ema tax fxn that broke vignettes

--- R/get_ema_taxonomy.R
+++ R/get_ema_taxonomy.R
@@ -5,14 +5,14 @@
 #' @export
 
 
-get_ema_taxonomy <- function() get_ema_taxonomy <- function() {
+get_ema_taxonomy <- function() {
   url <- "https://apex.psmfc.org/akfin/data_marts/ema/lut_trawl_species_tsn?"
-  
+
   # Fetch as UTF-8 text (note: 'as', not 'type')
   resp_txt <- httr::content(httr::GET(url), as = "text", encoding = "UTF-8")
-  
+
   dat <- jsonlite::fromJSON(resp_txt, flatten = TRUE)
-  
+
   # Replace U+00BF with regular spaces in character columns
   dat <- dplyr::mutate(
     dat,
@@ -21,7 +21,7 @@ get_ema_taxonomy <- function() get_ema_taxonomy <- function() {
       ~ gsub("\u00BF", " ", .x, fixed = TRUE)
     )
   )
-  
+
   dat <- dplyr::rename_with(dat, tolower)
 
   return(dat)
